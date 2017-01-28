ef60c81acd25fc0f9f09898bc9e31b0f9823a700
Juude <juude.song@gmail.com>
2017-01-28 17:11:28 +0800

    a rich text editor according to cs139p

--- learnIOS/ViewController.m
+++ learnIOS/ViewController.m
@@ -10,13 +10,34 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextView *textContent;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
 @end
 
 @implementation ViewController
+- (IBAction)changeBackgroundAccordingToButton:(UIButton *)sender {
+    [self.textContent.textStorage addAttribute:NSForegroundColorAttributeName
+                                         value:sender.backgroundColor
+                                         range:self.textContent.selectedRange];
+}
+
+- (IBAction)outlineBodySection:(id)sender {
+    [self.textContent.textStorage addAttributes:@{
+                    NSStrokeWidthAttributeName:@-3,
+                    NSStrokeColorAttributeName: [UIColor blackColor]
+                    }
+                    range:self.textContent.selectedRange];
+}
+
+- (IBAction)unoutlineBodySection:(id)sender {
+    [self.textContent.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.textContent.selectedRange];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.titleLabel setText:@"文本编辑器"];
 }
 
 
